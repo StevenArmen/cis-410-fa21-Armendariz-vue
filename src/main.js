191defae14bdf03e9ba7578e563a9abb382d85ce
StevenArmen <63573515+StevenArmen@users.noreply.github.com>
2021-12-07 23:38:04 -0700

    deployment

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from "axios"
 
-import myRouter from "./Routes.js"
-
+import myRouter from "./routes.js"
+import theStore from "./store.js"
 axios.defaults.baseURL = "https://cis410-fa21-arevalo-api.azurewebsites.net"
 
 const myApp = createApp(App);
 myApp.use(myRouter);
+myApp.use(theStore);
 myApp.mount('#app');
 
